92e8f8123385a8c66fade50c637ba5417de998f0
mlinarrozinger <mlinarrozingerpeter@yahoo.com>
2016-12-01 21:15:36 +0100

    jinja, post in get

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import jinja2
 import webapp2
 
+import random
+
+from models import Sporocilo
+
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -26,6 +30,24 @@ class BaseHandler(webapp2.RequestHandler):
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
 
+class RezultatHandler(BaseHandler):
+    def post(self):
+        besedilo = self.request.get("sporocilo")
+
+        ime = self.request.get("ime")
+
+        sporocilo = Sporocilo(besedilo_sporocila=besedilo, avtor=ime)
+        sporocilo.put()
+
+        izzrebane = []
+
+        for j in range(7):
+            izzrebane.append(random.randint(1, 37))
+
+        params = {"name": ime, "besedilo": besedilo,
+                  "izzrebane": izzrebane}
+
+        return self.render_template("rezultat.html", params=params)
 
 class MainHandler(BaseHandler):
     def get(self):
@@ -45,13 +67,65 @@ class ContactHandler(BaseHandler):
         return self.render_template("contact.html")
 
 class HelloHandler(BaseHandler):
-    def gef(self):
+    def get(self):
         return self.render_template("hello.html")
+
+class SporocilaHandler(BaseHandler):
+    def get(self):
+
+        sporocila = Sporocilo.query().fetch()
+
+        params = {"sporocila": sporocila}
+
+        return self.render_template("sporocila.html", params=params)
+
+class PosameznoSporociloHandler(BaseHandler):
+    def get(self, sporocilo_id):
+
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+
+        params= {"sporocilo": sporocilo}
+
+        return self.render_template("posamezno_sporocilo.html", params=params)
+
+class EditHandler(BaseHandler):
+    def get(self, sporocilo_id):
+
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+
+        params = {"sporocilo": sporocilo}
+
+        return self.render_template("uredi_sporocilo.html", params=params)
+
+    def post(self, sporocilo_id):
+
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+
+        sporocilo.besedilo_sporocila = self.request.get("text-sporocila")
+
+        sporocilo.avtor = self.request.get("ime")
+
+        sporocilo.put()
+
+        return self.redirect_to("seznam_sporocil")
+
+
+
+
+
+
+
+
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route("/mywebsite.html", OsnovnaStran),
     webapp2.Route("/about", AboutHandler),
     webapp2.Route("/contact", ContactHandler),
-    webapp2.Route("/hello", HelloHandler)
+    webapp2.Route("/hello", HelloHandler),
+    webapp2.Route('/rezultat', RezultatHandler),
+    webapp2.Route('/sporocila', SporocilaHandler, name="seznam_sporocil"),
+    webapp2.Route("/sporocila/<sporocilo_id:\d+>", PosameznoSporociloHandler),
+    webapp2.Route("/sporocila/<sporocilo_id:\d+>/uredi", EditHandler)
+
 ], debug=True)
---
+++ models.py
@@ -0,0 +1,6 @@
+from google.appengine.ext import ndb
+
+class Sporocilo(ndb.Model):
+    besedilo_sporocila = ndb.StringProperty()
+    avtor = ndb.StringProperty()
+    poslano = ndb.DateTimeProperty(auto_now_add=True)
